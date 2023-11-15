21b534db98d2d54cf4993b16f2c59f0e1cc0fefa
nthskyradiated <andy.pandaan@outlook.com>
2023-11-15 14:59:00 +0400

    working on frontend

---
+++ client/src/components/Spinner.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { ConicGradient } from '@skeletonlabs/skeleton';
+// import type { ConicStop } from '@skeletonlabs/skeleton';
+    const conicStops = [
+        { color: 'transparent', start: 0, end: 25 },
+        { color: 'rgb(var(--color-primary-500))', start: 75, end: 100 }
+    ];
+</script>
+
+<ConicGradient stops={conicStops} spin>Loading</ConicGradient>
--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,5 +1,19 @@
 <script>
   import "../app.postcss";
+  import { AppShell, AppBar,LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+  <AppBar>
+    <LightSwitch />
+    <svelte:fragment slot="header">Header</svelte:fragment>
+    <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+  </AppBar>
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
