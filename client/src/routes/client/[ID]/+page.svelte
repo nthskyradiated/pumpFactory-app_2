5aeb30251e2a5be62c136e24ea129d2c5b8a631d
nthskyradiated <andy.pandaan@outlook.com>
2023-11-22 17:02:25 +0400

    will start working on attendance queries

--- client/src/routes/client/[ID]/+page.svelte
+++ client/src/routes/client/[ID]/+page.svelte
@@ -2,6 +2,7 @@
   import { queryStore, gql, getContextClient } from '@urql/svelte';
   import Spinner from '../../../components/Spinner.svelte';
   import { TabGroup, Tab, getModalStore } from '@skeletonlabs/skeleton';
+  import {tabSet} from '$lib/utilsStore'
 
 export let data
 let {ID} = data
@@ -50,7 +51,6 @@ const updateModal = {
 	type: 'component',
   component: 'updateClientModal'
 };
-let tabSet = 3
 
 console.log(singleClient);
 			
@@ -68,15 +68,15 @@ console.log(singleClient);
 
 <div class="card p-4 mt-8">
   <TabGroup>
-    <Tab bind:group={tabSet} name="tab1" value={0}>
+    <Tab bind:group={$tabSet} name="tab1" value={0}>
       <svelte:fragment slot="lead">(icon)</svelte:fragment>
       <span>Client Details</span>
     </Tab>
-    <Tab bind:group={tabSet} name="tab2" value={1}>Product Enrolment</Tab>
-    <Tab bind:group={tabSet} name="tab3" value={2}>Sessions</Tab>
+    <Tab bind:group={$tabSet} name="tab2" value={1}>Product Enrolment</Tab>
+    <Tab bind:group={$tabSet} name="tab3" value={2}>Sessions</Tab>
     <!-- Tab Panels --->
     <svelte:fragment slot="panel">
-      {#if tabSet === 0}
+      {#if $tabSet === 0}
       <h1 class='h4 mb-1'>Id:</h1><h1 class='h5 mb-1'>{$getClient.data.client.id}</h1>
       <h1 class='h4 mb-1'>Name:</h1><h1 class='h5 mb-1'>{$getClient.data.client.name}</h1>
       <h1 class='h4 mb-1'>Email:</h1><h1 class='h5 mb-1'>{$getClient.data.client.email}</h1>
@@ -85,14 +85,14 @@ console.log(singleClient);
       <h1 class='h4 mb-1'>Age:</h1><h1 class='h5 mb-1'>{$getClient.data.client.age}</h1>
       <h1 class='h4 mb-1'>Status:</h1><h1 class='h5 mb-1'>{$getClient.data.client.membershipStatus}</h1>
       <h1 class='h4 mb-1'>Waiver:</h1><h1 class='h5 mb-1'>{$getClient.data.client.waiver}</h1>
-      {:else if tabSet === 1}
+      {:else if $tabSet === 1}
       {#if $getClient.data.client.product}
       <h1 class='h4 mb-1'>Product Name:</h1><h1 class='h5 mb-1'>{$getClient.data.client.product.name}</h1>
       <h1 class='h4 mb-1'>Product Description:</h1><h1 class='h5 mb-1'>{$getClient.data.client.product.description}</h1>
       {:else}
       <h1 class='h5 mb-1'>Client not enrolled in any product</h1>
       {/if}
-      {:else if tabSet === 2}
+      {:else if $tabSet === 2}
       {#if !$getClient.data.client.attendance || 'undefined'}
       <h1 class='h5 mb-1'>No attendance history for this client</h1>
       
