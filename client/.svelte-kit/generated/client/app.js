99d17be03e09a41e281380a463eff52594adbf80
nthskyradiated <andy.pandaan@outlook.com>
2024-01-22 21:08:34 +0400

    working on SSR

--- client/.svelte-kit/generated/client/app.js
+++ client/.svelte-kit/generated/client/app.js
@@ -18,7 +18,7 @@ export const server_loads = [];
 export const dictionary = {
 		"/": [2],
 		"/client/[ID]": [3],
-		"/dashboard": [~4],
+		"/dashboard": [4],
 		"/products": [6],
 		"/product/[ID]": [5],
 		"/result/[searchValue]": [7],
--- client/src/routes/dashboard/+page.server.ts
+++
@@ -1,5 +0,0 @@
-export async function load ({ cookies, locals, request }) {
-
-	 cookies.get('refreshToken')
-	//  request.headers.set('Authorization', `Bearer ${locals.token}`);
-}
